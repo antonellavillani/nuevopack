9890814763e4a6c490626c8cab6261d412594f19
Lara Antonella Villani Magrini <lara.villani@davinci.edu.ar>
2025-05-19 15:13:59 -0300

    refactor: Modificación en la conexión con la base de datos MySQL en config.php

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
 <?php
 // Configuración de conexión a la base de datos
-$host = 'localhost'; // o la dirección IP del servidor de la base de datos
+$host = 'localhost';
 $dbname = 'nuevopack_db';
-$username = 'root'; // Reemplaza con el nombre de usuario de tu base de datos
-$password = 'toor'; // Reemplaza con la contraseña de tu base de datos
+$username = 'root';
+$password = 'toor';
 
 try {
-    // Crear una instancia de PDO
+    // Instancia de PDO
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 
-    // Configurar el modo de error de PDO para excepciones
+    // Configuración del modo de error de PDO para excepciones
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
