237832a7d4179384b6130b9d34e7d52d202ce4f9
Lara Antonella Villani Magrini <lara.villani@davinci.edu.ar>
2025-08-07 16:46:50 -0300

    style: Mejora en el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,32 @@
 <footer>
     <div class="footer-content">
         <!-- Información de contacto -->
-        <div class="informacion-contacto">
+        <div class="informacion-contacto footer-left">
             <h4>Contacto</h4>
-            <p>Dirección: 237 P. Elustondo, Quilmes, Buenos Aires Argentina</p>
             <p>Teléfono: 11 3932-7709 (Daniel)</p>
             <p>Email: nuevopack@gmail.com</p>
             <p>Horario: Lunes a Viernes, 9:00 a 18:00. Sábados, 9:00 a 13:00.</p>
         </div>
 
         <!-- Mapa del sitio -->
-        <div>
+        <div class="footer-section footer-right">
             <h4>Mapa del Sitio</h4>
-            <ul class="liststyle_none">
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="servicios.php">Servicios</a></li>
-                <li><a href="quienes_somos.php">Quiénes Somos</a></li>
-                <li><a href="contacto.php">Contáctanos</a></li>
+            <ul class="liststyle_none mapa-sitio">
+                <li><a href="index.php"><i class="fa-duotone fa-solid fa-house"></i>Inicio</a></li>
+                <li><i class="fa-duotone fa-solid fa-list"></i>
+                    <span>Servicios</span>
+                    <ul class="sublista-servicios liststyle_none">
+                        <li><a href="ficha_servicio.php?id=1"><i class="fa-duotone fa-solid fa-print"></i>Impresión</a></li>
+                        <li><a href="ficha_servicio.php?id=2"><i class="fa-duotone fa-solid fa-paint-roller"></i>Barniz</a></li>
+                        <li><a href="ficha_servicio.php?id=3"><i class="fa-duotone fa-solid fa-scissors"></i>Troquelado</a></li>
+                        <li><a href="ficha_servicio.php?id=4"><i class="fa-duotone fa-solid fa-box"></i>Pegado de estuches</a></li>
+                        <li><a href="ficha_servicio.php?id=5"><i class="fa-duotone fa-solid fa-calendar"></i>Almanaques</a></li>
+                    </ul>
+                </li>
+                <li><a href="quienes_somos.php"><i class="fa-duotone fa-solid fa-user-group"></i>Quiénes Somos</a></li>
+                <li><a href="contacto.php"><i class="fa-duotone fa-solid fa-phone"></i>Contáctanos</a></li>
             </ul>
+            </div>
         </div>
     </div>
 
