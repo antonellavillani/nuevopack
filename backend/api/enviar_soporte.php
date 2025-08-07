95fa87797b757af3ad41a6fd0a94c88ebf144158
Lara Antonella Villani Magrini <lara.villani@davinci.edu.ar>
2025-08-07 12:26:39 -0300

    feat: Agregué el endpoint enviar_soporte.php para soporte desde la app

---
+++ backend/api/enviar_soporte.php
@@ -0,0 +1,54 @@
+<?php
+require_once '../../config/config.php';
+require_once '../../admin-xyz2025/config_secrets.php';
+
+require_once '../PHPMailer-master/PHPMailer-master/src/Exception.php';
+require_once '../PHPMailer-master/PHPMailer-master/src/PHPMailer.php';
+require_once '../PHPMailer-master/PHPMailer-master/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header('Content-Type: application/json');
+
+$input = json_decode(file_get_contents('php://input'), true);
+$asunto  = $input['asunto'] ?? '';
+$mensaje = $input['mensaje'] ?? '';
+$imagen  = $input['imagen'] ?? ''; 
+
+if(!$asunto || !$mensaje){
+    echo json_encode(['ok' => false, 'msg' => 'Faltan campos']);
+    exit;
+}
+
+try {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host       = SMTP_HOST;
+    $mail->SMTPAuth   = true;
+    $mail->Username   = SMTP_USER_SOPORTE;
+    $mail->Password   = SMTP_PASSWORD_SOPORTE;
+    $mail->Port       = SMTP_PORT;
+
+    $mail->setFrom(SMTP_USER_SOPORTE, 'NuevoPack Admin App');
+    $mail->addAddress(SMTP_USER_SOPORTE, 'Soporte NuevoPack');
+
+    $mail->Subject = $asunto;
+    $mail->Body = $mensaje;
+
+    // Si se adjunta foto
+    if($imagen){
+        $base64 = $imagen;
+        $data = base64_decode($base64);
+        $temp = tempnam(sys_get_temp_dir(), 'img_').'.jpg';
+        file_put_contents($temp, $data);
+        $mail->addAttachment($temp, 'captura.jpg');
+    }
+
+    $mail->send();
+    echo json_encode(['ok' => true]);
+
+} catch (Exception $e) {
+    error_log("Error PHPMailer: " . $e->getMessage());
+    echo json_encode(['ok' => false, 'msg' => $e->getMessage()]);
+}
