b0d6e2ee7b701921e80ca65115d3cc64b5ea7f3f
Lara Antonella Villani Magrini <lara.villani@davinci.edu.ar>
2025-08-14 19:52:41 -0300

    feat: Agregué botón para volver a la web en el panel de administrador

--- admin-xyz2025/dashboard.php
+++ admin-xyz2025/dashboard.php
@@ -29,6 +29,7 @@ include ("includes/header.php");
             <a href="#" id="btn-mi-cuenta" class="mi-cuenta">Mi cuenta</a>
         </div>
         <div class="nav-item"><a href="soporte.php" class="soporte">Soporte</a></div>
+        <div class="nav-item"><a href="../index.php" class="volver-pagina">Volver a la web</a></div>
     </div>
 
     <!-- Modal Mi Cuenta -->
